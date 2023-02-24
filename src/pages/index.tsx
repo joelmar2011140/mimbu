7d7280692dc7882cb7f5e9bffa4b680605a799bd
Joel Marinho <joeldeveloper2011140@gmail.com>
2023-02-24 15:57:59 +0100

    chore: setup some dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function PaginaInicial() {
   return (
-    <div>index</div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
