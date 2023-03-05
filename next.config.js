6ad7245b58ab8f88b8a137c7770bc355719ac413
Joel Marinho <joeldeveloper2011140@gmail.com>
2023-03-06 00:54:21 +0100

    chore: setup env variable to database

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      { source: '/', destination: '/edicoes', permanent: true },
+      { source: '/dashboard', destination: '/dashboard/edicoes', permanent: true }
+    ]
+  }
 }
 
 module.exports = nextConfig
